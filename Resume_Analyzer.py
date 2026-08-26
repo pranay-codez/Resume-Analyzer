d21a194fd3e8cdad9e0c64e78dc36e8cc8b080c9
pranay-codez <pranaymenon37@gmail.com>
2026-08-26 10:55:48 +0530

    Created the structure using ollama python library

--- Resume_Analyzer.py
+++ Resume_Analyzer.py
@@ -1,26 +1,27 @@
-# Resume analyzer 
+# # Resume analyzer 
 
-import requests
-import json
-url = "http://localhost:11434/api/chat"
-params = {
-    "model" : "llama3.2",
-    "messages" : [
-        {
-            "role" : "user",
-            "content": "what is a list in python?"
-        }
-    ],
-    "stream" : False
+# import requests
+# import json
+# url = "http://localhost:11434/api/chat"
+# params = {
+#     "model" : "llama3.2",
+#     "messages" : [
+#         {
+#             "role" : "user",
+#             "content": "what is a list in python?"
+#         }
+#     ],
+#     "stream" : False
 
-}
+# }
 
-response = requests.post(url, json = params)
-check = response.status_code
-print(check)
-if(check == 200):
-    data = response.json()
-    with open("Report.txt", "w") as file:
-        json.dump(data, file, indent=4)
-    print(data["message"]["content"])
+# response = requests.post(url, json = params)
+# check = response.status_code
+# print(check)
+# if(check == 200):
+#     data = response.json()
+#     with open("Report.txt", "w") as file:
+#         json.dump(data, file, indent=4)
+#     print(data["message"]["content"])
   
+from ollama import chat
